9a1e37182adb7bf8bccff639110635c85c4369a4
none <salr3467@gmail.com>
2017-05-15 09:58:01 -0400

    setup database, created initial game migration, and created game controller with new game route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  get 'games/new', to: "games#new"
+
+  resources :games
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
